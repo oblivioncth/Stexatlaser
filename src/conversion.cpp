92329ff726737f2cca03ed69b1ed24cade749805
Christian Heimlich <chris@pcserenity.com>
2021-09-25 14:07:17 -0400

    Add first image conversion skeleton

--- src/conversion.cpp
+++ src/conversion.cpp
@@ -1,1 +1,21 @@
 #include "conversion.h"
+
+//===============================================================================================================
+// TO_TEX_CONVERTER
+//===============================================================================================================
+
+//-Constructor-------------------------------------------------------------------------------------------------
+ToTexConverter::ToTexConverter(const QImage& sourceImage, const Options& options) :
+    mSourceImage(sourceImage),
+    mOptions(options)
+{}
+
+//-Instance Functions--------------------------------------------------------------------------------------------
+//Private:
+
+
+//Public:
+KTex ToTexConverter::convert()
+{
+    return KTex();
+}
--- src/conversion.h
+++ src/conversion.h
@@ -1,5 +1,43 @@
 #ifndef CONVERSION_H
 #define CONVERSION_H
 
+#include <QImage>
+
+#include "klei/k-tex.h"
+
+class ToTexConverter
+{
+//-Structs----------------------------------------------------------------------------------------------------------
+public:
+    struct Options
+    {
+        KTex::Header::Platform platform = KTex::Header::Platform::PC;
+        KTex::Header::TextureType textureType = KTex::Header::TextureType::TwoD;
+        KTex::Header::PixelFormat pixelType = KTex::Header::PixelFormat::DXT5;
+        bool generateMipMaps = true;
+        bool premultiplyAlpha = true;
+        //Type resampleFilter = default;
+    };
+
+//-Class Members----------------------------------------------------------------------------------------------------
+private:
+
+//-Instance Members-------------------------------------------------------------------------------------------------
+private:
+    const QImage& mSourceImage;
+    const Options& mOptions;
+
+//-Constructor-------------------------------------------------------------------------------------------------------
+public:
+    ToTexConverter(const QImage& sourceImage, const Options& options);
+
+//-Instance Functions----------------------------------------------------------------------------------------------
+private:
+    //Type convertMainImage();
+
+public:
+    KTex convert();
+
+};
 
 #endif // CONVERSION_H
