5b2fca9677fd0ddc31102d19b8a46715ef33fd9a
Christian Heimlich <chris@pcserenity.com>
2021-12-09 06:08:53 -0500

    Increment version

--- src/version.h
+++ src/version.h
@@ -1,11 +1,11 @@
 #ifndef VERSION_H
 #define VERSION_H
 
-#define VER_FILEVERSION             0,2,0,1
-#define VER_FILEVERSION_STR         "0.2.0.1"
+#define VER_FILEVERSION             0,2,0,2
+#define VER_FILEVERSION_STR         "0.2.0.2"
 
-#define VER_PRODUCTVERSION          0,2,0,1
-#define VER_PRODUCTVERSION_STR      "0.2.0.1"
+#define VER_PRODUCTVERSION          0,2,0,2
+#define VER_PRODUCTVERSION_STR      "0.2.0.2"
 
 #define VER_COMPANYNAME_STR         "oblivioncth"
 #define VER_FILEDESCRIPTION_STR     "Klei TEX Atlaser"
